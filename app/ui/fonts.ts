d614409022f0f107757db0e708c88905927940d0
bygerman <by.german@outlook.com>
2024-02-25 13:09:48 -0500

    Add secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
 // Next permits add fonts easily! (next have added google fonts)
-import { Inter } from 'next/font/google'
+import { Inter, Lusitana } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
