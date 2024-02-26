c2a9254cffae65216b2ec93a17624c0b63f50581
bygerman <by.german@outlook.com>
2024-02-26 18:38:07 -0500

    Pattern: Showing active links
    A common UI pattern is to show an active link to indicate to the user what page they are currently on

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,12 @@
+'use client' // usePathname only works in Client Components
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+import { usePathname } from 'next/navigation'; 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -18,17 +20,23 @@ const links = [
 ];
 
 export default function NavLinks() {
+  // usePathname only works in Client Components (add 'use client' at the top of the file)
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
-        
+
         //  <Link> allows you to do client-side navigation with JavaScript, like a single-page application.
         return (
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3
+            
+            ${pathname === link.href ? 'bg-sky-100 text-blue-600' : ''}
+            `}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
